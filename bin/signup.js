512fdaf7e025ec52e2829c9bd18fefe46b9b4a3b
Alekos Filini <alekos.filini@gmail.com>
2014-06-05 16:17:04 +0200

    Various changes

--- bin/signup.js
+++ bin/signup.js
@@ -5,7 +5,7 @@ var http = require('http'),
 
 	pubkey    = '6LddovQSAAAAAKBR27JXnpZJYXMLJI7JsHzm0p1V',
 	privkey   = '6LddovQSAAAAAGU9JLWXFsmAGMEsPmgNUsSkP-bR',
-	addr      = '192.168.1.103',
+	addr      = '192.168.1.141',
 	port      = 7761, // 0x77 0x61 = "wa" :3
 	server = http.createServer(function (req, res) {
 		if (req.method === 'GET') {
